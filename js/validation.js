a50e9132082c1a0692447a225ddb22e72a452441
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-23 15:06:25 +0000

    Basic JS validation

---
+++ js/validation.js
@@ -0,0 +1,12 @@
+/* @ Stephen O'Connor */
+(function () {
+
+   validateForm = function () {
+      for (var i = 0; i < myform.elements.length; i++) {
+         if (myform.elements[i].className === "req" && myform.elements[i].value.length === 0) {
+            alert('Please fill in all required fields');
+            return false;
+         }
+      };
+   };
+}());
